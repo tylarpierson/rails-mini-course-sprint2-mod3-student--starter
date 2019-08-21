da2d2ad6e72ef63b237fbb5244857e6896af2ee1
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-21 16:14:40 -0400

    validated products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   has_many :order_products
   has_many :orders through :order_products
 
+  validates :name, :cost_cents >= 0, :inventory >= 0, presence: true
+
   def available?
     inventory > 0
   end
