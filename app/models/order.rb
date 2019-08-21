93ea774fa47ef838cdac70795325584e40db1d0d
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-21 16:00:37 -0400

    took out uneccessary products instance

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,6 @@ class Order < ApplicationRecord
   has_many :products through :order_products
   belongs_to :customers
 
-  def products
-    product_ids = OrderProduct.where(order_id: id).pluck(:product_id)
-    Product.find(product_ids)
-  end
-
   def shippable?
     status != "shipped" && products.count >= 1
   end
