8e91f552d2223c95691e5e5714772aecc0bf5e96
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-21 16:49:56 -0400

    seperated validations onto separate lines

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,11 @@ class Product < ApplicationRecord
   has_many :order_products
   has_many :orders through :order_products
 
-  validates :name, :cost_cents >= 0, :inventory >= 0, presence: true
+  validates :name, presence: true
+  validates :cost_cents, presence: true, greater_than: 0
+  validates :inventory, presence: true, greater_than_or_equal_to: 0
 
-  
+
 
   def available?
     inventory > 0
