f2f213da91ea74b66b0c16dda211261686d0489a
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-21 16:11:24 -0400

    simplified index and create controller methods

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,8 +3,7 @@ module Api
     class ProductsController < ApplicationController
       def index
         if params[:order_id].present?
-          product_ids = OrderProduct.where(order_id: params[:order_id]).pluck(:product_id)
-          @products = Product.find(product_ids)
+          @products = Product.find(product_ids).order_id
         else
           @products = Product.where("inventory > ?", 0).order(:cost)
         end
@@ -20,7 +19,7 @@ module Api
 
       def create
         @order = Order.find(params[:order_id])
-        @order_product = OrderProduct.new(order_id: @order.id, product_id: order_product_params[:product_id])
+        @order_product = OrderProduct.build(order_id)
 
         if @order_product.save
           render json: @order, status: :created, location: api_v1_order_url(@order)
